5c195d81baa771cbed1ba5996d08e0ad8b6d308e
mudasser ajaz <mudasserajaz@mudassers-MacBook-Air.local>
2019-01-29 00:24:46 +0100

    added setup with react, babel and webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const wrapper = document.getElementById("index");
+wrapper ? ReactDOM.render(<div>app</div>, wrapper) : false;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    });
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    htmlPlugin
+  ]
+};
